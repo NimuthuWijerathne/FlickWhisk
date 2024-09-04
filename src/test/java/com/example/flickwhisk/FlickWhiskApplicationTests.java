8734d15379e5a8b2ff0ae06a09f1a884cc6b909a
NimuthuWijerathne <73591033+NimuthuWijerathne@users.noreply.github.com>
2024-09-04 17:53:08 +0530

    initial commit

---
+++ src/test/java/com/example/flickwhisk/FlickWhiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flickwhisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlickWhiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
